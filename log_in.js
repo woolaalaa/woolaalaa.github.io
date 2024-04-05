2cfe87cc8515356a26d892ab7cdb7ff735a19681
p269228 <p269228@gmail.com>
2024-04-05 20:28:29 +0800

    login test

--- log_in.js
+++ log_in.js
@@ -4,13 +4,20 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
 
+  // 構造請求的數據
+  var data = {
+    'username': username,
+    'password': password
+  };
+
   fetch('https://script.google.com/macros/s/AKfycbwhFqd22TyW4j4QzDkAosOkNNbIrEFCYIWMnM44Mjwi1X22Cpq9b9Bq80_PQus7A7DSVw/exec', {
     method: 'POST',
     mode: 'no-cors',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      'Content-Type': 'application/json'
     },
-    body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
+    // 將對象轉換為JSON字符串
+    body: JSON.stringify(data)
   })
   .then(response => response.text())
   .then(text => alert(text))
